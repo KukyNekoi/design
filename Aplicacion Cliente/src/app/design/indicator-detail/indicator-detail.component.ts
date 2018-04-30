a4b77a032b8312038a8a3bb0ea3e61c3a86614a8
kuky.nekoi@gmail.com <jvarred@gmail.com>
2018-04-30 02:46:01 -0300

    fix(detail_compoent): fixed indentation

--- Aplicacion Cliente/src/app/design/indicator-detail/indicator-detail.component.ts
+++ Aplicacion Cliente/src/app/design/indicator-detail/indicator-detail.component.ts
@@ -9,33 +9,108 @@ export class IndicatorDetailComponent implements OnInit {
   public data: any[];
   public filters: any[];
 
-  constructor() { 
-    this.data = [{
-      option: [
-        {name: 'Barras'},{name: 'Líneas'},{name: 'Circular'}
-      ]
-    }];
+  constructor() {
+    this.data = [
+      {
+        option: [{ name: 'Barras' }, { name: 'Líneas' }, { name: 'Circular' }]
+      }
+    ];
 
-    this.filters = [ { title: 'intervencion', key: '#intervencion',
-    content: [{ name: 'nota de prensa 1', value: '', eventDate: 'dd-mm-aaaa', description: 'Es una descripción de un recurso',
-              	resources:   [{rname: 'documento 1', rtype:'Tipo1', rdate: 'dd-mm-aaaa', rlink: '#'},
-	              {rname: 'documento 2', rtype:'Tipo2', rdate: 'dd-mm-aaaa', rlink: '#'}]	
-    },{ name: 'nota de prensa 2', value: '456', eventDate: 'dd-mm-aaaa', description: 'Es una descripción de otro recurso',
-	      resources: [{rname: 'documento 45', rtype:'Tipo1', rdate: 'dd-mm-aaaa', rlink: '#'},
-	      {rname: 'documento 2', rtype:'Tipo2', rdate: 'dd-mm-aaaa', rlink: '#'}]	
-      }]  
-    },{ title: 'diagnostico', key: '#diagnostico',
-    content: [{ name: 'documento de diagnostico', value: '', eventDate: 'dd-mm-aaaa', description: 'Es una descripción de un recurso',
-                resources: [{rname: 'documento 1', rtype:'Tipo1', rdate: 'dd-mm-aaaa', rlink: '#'},
-                {rname: 'documento 2', rtype:'Tipo2', rdate: 'dd-mm-aaaa', rlink: '#'}]	
-    },{ name: 'nota de prensa 2', value: '456', eventDate: 'dd-mm-aaaa', description: 'Es una descripción de otro recurso',
-        resources: [{rname: 'documento 45', rtype:'Tipo1', rdate: 'dd-mm-aaaa', rlink: '#'},
-        {rname: 'documento 2', rtype:'Tipo2', rdate: 'dd-mm-aaaa', rlink: '#'}]	
-    }]  
-  }];
+    this.filters = [
+      {
+        title: 'intervencion',
+        key: '#intervencion',
+        content: [
+          {
+            name: 'nota de prensa 1',
+            value: '',
+            eventDate: 'dd-mm-aaaa',
+            description: 'Es una descripción de un recurso',
+            resources: [
+              {
+                rname: 'documento 1',
+                rtype: 'Tipo1',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              },
+              {
+                rname: 'documento 2',
+                rtype: 'Tipo2',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              }
+            ]
+          },
+          {
+            name: 'nota de prensa 2',
+            value: '456',
+            eventDate: 'dd-mm-aaaa',
+            description: 'Es una descripción de otro recurso',
+            resources: [
+              {
+                rname: 'documento 45',
+                rtype: 'Tipo1',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              },
+              {
+                rname: 'documento 2',
+                rtype: 'Tipo2',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        title: 'diagnostico',
+        key: '#diagnostico',
+        content: [
+          {
+            name: 'documento de diagnostico',
+            value: '',
+            eventDate: 'dd-mm-aaaa',
+            description: 'Es una descripción de un recurso',
+            resources: [
+              {
+                rname: 'documento 1',
+                rtype: 'Tipo1',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              },
+              {
+                rname: 'documento 2',
+                rtype: 'Tipo2',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              }
+            ]
+          },
+          {
+            name: 'nota de prensa 2',
+            value: '456',
+            eventDate: 'dd-mm-aaaa',
+            description: 'Es una descripción de otro recurso',
+            resources: [
+              {
+                rname: 'documento 45',
+                rtype: 'Tipo1',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              },
+              {
+                rname: 'documento 2',
+                rtype: 'Tipo2',
+                rdate: 'dd-mm-aaaa',
+                rlink: '#'
+              }
+            ]
+          }
+        ]
+      }
+    ];
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
