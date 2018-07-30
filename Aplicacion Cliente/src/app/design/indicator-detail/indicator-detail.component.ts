257e9420a186b42be634743396245f64094660cf
Unknown <yorch5.77@gmail.com>
2018-07-30 00:42:49 -0400

    Auto stash before revert of "Auto stash before merge of "Dani" and "origin/Dani""
    combinación de algunos cambios de Dani y Mios

--- Aplicacion Cliente/src/app/design/indicator-detail/indicator-detail.component.ts
+++ Aplicacion Cliente/src/app/design/indicator-detail/indicator-detail.component.ts
@@ -17,6 +17,7 @@ export class IndicatorDetailComponent implements OnInit {
   public editIndicatorModalRef: BsModalRef;
   public editMetaModalRef: BsModalRef;
   public alertModalRef: BsModalRef;
+  public editDocumentModalRef: BsModalRef;
 
   constructor(private modalService: BsModalService) {
     this.data = [
@@ -150,6 +151,10 @@ export class IndicatorDetailComponent implements OnInit {
 
   openModalAlert(template: TemplateRef<any>) {
     this.alertModalRef = this.modalService.show(template, {backdrop: 'static'});
+  }
+
+  openmodaleditDocument(template: TemplateRef<any>) {
+    this.editDocumentModalRef = this.modalService.show(template, {backdrop: 'static'});
   }
 
   ngOnInit() {}
