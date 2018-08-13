92518d9db018497c70da5c604b7d19bca732ff0c
Alisboa13 <AlanDeniLisboa@Gmail.com>
2018-08-13 15:11:37 -0300

    package

---
+++ Aplicacion Cliente/src/app/design/indicator-detail/indicator-alert/indicator-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indicator-alert',
+  templateUrl: './indicator-alert.component.html',
+  styleUrls: ['./indicator-alert.component.css']
+})
+export class IndicatorAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
