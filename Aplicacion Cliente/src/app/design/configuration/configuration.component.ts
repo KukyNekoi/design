f8162e6e204fae5f8a93f46a43a56b071300422c
Daniela Rossana Paredes <elynawatcher@gmail.com>
2018-08-17 02:18:29 -0300

    Feat(modal configurations)
    - Create Indicator Group
    - Edit Indicator Group
    - Delete Indicator Group
    - Create Indicator
    - Edit Indicator
    - Delete Indicator

--- Aplicacion Cliente/src/app/design/configuration/configuration.component.ts
+++ Aplicacion Cliente/src/app/design/configuration/configuration.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-configuration',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuration.component.css']
 })
 export class ConfigurationComponent implements OnInit {
+  @Input() public recordFilter: any;
+  public createGroupModalRef: BsModalRef;
+  public editGroupModalRef: BsModalRef;
+  public deleteGroupModalRef: BsModalRef;
+  public createIndicatorModalRef: BsModalRef;
+  public editIndicatorModalRef: BsModalRef;
+  public deleteIndicatorModalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { 
+    console.log(this.recordFilter);
+  }
 
   ngOnInit() {
+  }
+
+  openModalCreateGroup(template: TemplateRef<any>) {
+    this.createGroupModalRef = this.modalService.show(template, {backdrop: 'static'});
+  }
+
+  openModalEditGroup(template: TemplateRef<any>) {
+    this.editGroupModalRef = this.modalService.show(template, {backdrop: 'static'});
+  }
+
+  openModalDeleteGroup(template: TemplateRef<any>) {
+    this.deleteGroupModalRef = this.modalService.show(template, {backdrop: 'static'});
+  }
+
+  openModalCreateIndicator(template: TemplateRef<any>) {
+    this.createIndicatorModalRef = this.modalService.show(template, {backdrop: 'static'});
+  }
+
+  openModalEditIndicator(template: TemplateRef<any>) {
+    this.editIndicatorModalRef = this.modalService.show(template, {backdrop: 'static'});
+  }
+
+  openModalDeleteIndicator(template: TemplateRef<any>) {
+    this.deleteIndicatorModalRef = this.modalService.show(template, {backdrop: 'static'});
   }
 
 }
